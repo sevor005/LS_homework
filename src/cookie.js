a657fde58ab64ca20b83473ab2601bb594f4d38b
sevor005 <sevor005@yandex.ru>
2018-11-27 13:25:39 +0700

    cookie finish

--- src/cookie.js
+++ src/cookie.js
@@ -43,10 +43,100 @@ const addButton = homeworkContainer.querySelector('#add-button');
 // таблица со списком cookie
 const listTable = homeworkContainer.querySelector('#list-table tbody');
 
-filterNameInput.addEventListener('keyup', function() {
-    // здесь можно обработать нажатия на клавиши внутри текстового поля для фильтрации cookie
+filterNameInput.addEventListener('keyup', () => {
+  getAndRendersCookies();
 });
 
+const getAndRendersCookies = () => {
+  const filterValue = filterNameInput.value;
+  let cookies = [];
+
+  if(!filterValue) {
+    cookies = getCookies();
+  } else {
+    cookies = getFiltersCookies(filterValue);
+  }
+
+  renderCookies(cookies);
+};
+
+const addCookiesBrowser = (name, value) => {
+  document.cookie = `${name}=${value}`;
+};
+
+const getCookies = () => {
+  const cookieArrays = document.cookie.split('; ');
+
+  return cookieArrays.reduce((prev, elem) => {
+    const[name, value] = elem.split('=');
+    
+    if(name) prev.push({ name, value });
+    return prev;
+  }, []);
+};
+
+const deleteCookie = name => {
+  const data = new Date(0);
+
+  document.cookie = `${name}=;expires=${data.toUTCString()}`;
+};
+
+const getFiltersCookies = value => {
+  const cookies = getCookies();
+
+  return cookies.filter(cookie => isMatching(cookie.name, value) || isMatching(cookie.value, value));
+};
+
+const renderCookies = cookies => {
+  listTable.innerHTML = '';
+
+  cookies.forEach(cookie => {
+    const row = document.createElement('tr');
+    const cellName = document.createElement('td');
+    const cellValue = document.createElement('td');
+    const cellButton = document.createElement('td');
+    const button = document.createElement('button');
+
+    cellName.textContent = cookie.name;
+    cellValue.textContent = cookie.value;
+    button.textContent = 'удалить';
+    button.setAttribute('data-name', cookie.name);
+    
+    cellButton.appendChild(button);
+    row.appendChild(cellName);
+    row.appendChild(cellValue);
+    row.appendChild(cellButton);
+
+    listTable.appendChild(row);
+  });
+};
+
 addButton.addEventListener('click', () => {
-    // здесь можно обработать нажатие на кнопку "добавить cookie"
+  const name = addNameInput.value;
+  const value = addValueInput.value;
+
+  addCookiesBrowser(name, value);
+  getAndRendersCookies();
+
+  addNameInput.value = '';
+  addValueInput.value = '';
+});
+
+function isMatching(full, chunk) {
+  const string = full.toLowerCase();
+  const subString = chunk.toLowerCase();
+
+  if(string.indexOf(subString) != -1) {
+    return true;
+  }
+
+  return false;
+}
+
+listTable.addEventListener('click', e => {
+  if(e.target.tagName !== 'BUTTON') return;
+
+  const name = e.target.getAttribute('data-name');
+  deleteCookie(name);
+  e.target.closest('tr').remove();
 });
